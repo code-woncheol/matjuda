58b8cc30dbdbb67e7a996159970f87b9364b5758
code-woncheol <codewc23@gmail.com>
2024-03-19 17:00:08 +0900

    Added MenuCard Components, Data Cleaning Json

--- matjuda-app/src/App.js
+++ matjuda-app/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MenuCard from "./components/MenuCard/MenuCard";
+import MenuData from "./data/DataCleaning.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {<MenuCard />}
+      {<MenuCard />}
+      {<MenuCard />}
+      {<MenuCard />}
+      {<MenuCard />}
+      {<MenuCard />}
     </div>
   );
 }
